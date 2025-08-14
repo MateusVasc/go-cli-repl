d0c11984b4878fec94f19be6ff5059f8da4b3393
MateusVasc <mvaraujo2004@gmail.com>
2025-08-14 00:00:42 -0300

    feat: adds repl main loop

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/MateusVasc/go-cli-repl/utils"
+)
 
 func main() {
-	fmt.Println("Hey, Hiraeth")
+	for {
+		fmt.Print("GoCLI > ")
+		scn := bufio.NewScanner(os.Stdin)
+
+		if scn.Scan() {
+			input := scn.Text()
+			fmt.Printf("Your command was: %s\n", utils.CleanInput(input)[0])
+		}
+	}
 }
