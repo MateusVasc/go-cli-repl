3440023656475378c8b5adbc7f79b6edc20f99f8
MateusVasc <mvaraujo2004@gmail.com>
2025-08-14 11:21:07 -0300

    feat: adds logic to handle new commands

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/MateusVasc/go-cli-repl/registry"
 	"github.com/MateusVasc/go-cli-repl/utils"
 )
 
@@ -15,7 +16,19 @@ func main() {
 
 		if scn.Scan() {
 			input := scn.Text()
-			fmt.Printf("Your command was: %s\n", utils.CleanInput(input)[0])
+			val := utils.CleanInput(input)[0]
+
+			command, ok := registry.Commands[val]
+			if !ok {
+				fmt.Printf("Unknown command %s\n", val)
+			} else {
+				err := command.Callback()
+
+				if err != nil {
+					fmt.Printf("Error while running command %s: %v\n", command.Name, err)
+				}
+			}
+
 		}
 	}
 }
